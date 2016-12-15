2c7bcc5f1bcd0f07d6e0751c1ecf0aece0de92fc
orbisnull <a1p2m3@gmail.com>
2016-12-15 20:33:13 +0800

    change apt key format

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -3,13 +3,6 @@ class orbisnull_nginx::repository inherits orbisnull_nginx {
     location => 'http://nginx.org/packages/mainline/debian/',
     release  => 'jessie',
     repos    => 'nginx',
-    require => Exec['apt-key nginx'],
-  }
-
-  $key = '573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62'
-  exec { 'apt-key nginx':
-    path    => '/bin:/usr/bin',
-    onlyif  => "/bin/bash -c \"! /usr/bin/apt-key adv --list-public-keys --with-fingerprint --with-colons | grep -q ${key}\"",
-    command => 'wget -qO - http://nginx.org/keys/nginx_signing.key | apt-key add -'
+    key     => {'id'=>'573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62', 'source'=>'http://nginx.org/keys/nginx_signing.key'},
   }
 }
